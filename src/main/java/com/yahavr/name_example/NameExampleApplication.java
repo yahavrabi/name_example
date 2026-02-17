03c864f2dba6bde29ecb3e119f678bd14d498233
yahavrabi <yahavrabinovich@gmail.com>
2026-02-17 13:18:27 +0200

    17/2/2026 13:17 sout in main

--- src/main/java/com/yahavr/name_example/NameExampleApplication.java
+++ src/main/java/com/yahavr/name_example/NameExampleApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NameExampleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(NameExampleApplication.class, args);
+		System.out.println("=====> app running....");
 	}
-
+	
 }
