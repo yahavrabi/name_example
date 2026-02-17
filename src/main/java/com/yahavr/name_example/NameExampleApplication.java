937898a539547f80487e43516edba2cc97badf26
yahavrabi <yahavrabinovich@gmail.com>
2026-02-17 13:11:28 +0200

    first commit

---
+++ src/main/java/com/yahavr/name_example/NameExampleApplication.java
@@ -0,0 +1,13 @@
+package com.yahavr.name_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameExampleApplication.class, args);
+	}
+
+}
